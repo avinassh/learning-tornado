845b89ba4c2700c4263c57e5d34c7b5bdab9e16e
avinassh <avinashsajjan@gmail.com>
2013-12-12 14:34:35 +0530

    added notes

--- tornado-book/1-1-hello.py
+++ tornado-book/1-1-hello.py
@@ -1,3 +1,6 @@
+#!/bin/python
+
+# bunch of tornado imports
 import tornado.httpserver 
 import tornado.ioloop 
 import tornado.options 
@@ -8,13 +11,23 @@ define("port", default=8000, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler): 
     def get(self):
-        greeting = self.get_argument('greeting', 'Hello') 
-        self.write(greeting + ', friendly user!')
+        # get_argument seems a dictionary and it tries to get 
+        # value for key 'greeting' ?
+        greeting = self.get_argument('greeting', 'Hola!')
+
+        # No self.wfile.write, then how about self.rfile ? 
+        self.write(greeting + ',  dude!')
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
+
+    # seperate handlers? looks cool
     app = tornado.web.Application(handlers=[(r"/", IndexHandler)]) 
-    http_server = tornado.httpserver.HTTPServer(app) 
-    http_server.listen(options.port) 
+    http_server = tornado.httpserver.HTTPServer(app)
+
+    # listen on options port 
+    http_server.listen(options.port)
+    # start it 
     tornado.ioloop.IOLoop.instance().start()
+    # below thing won't stop
     tornado.ioloop.IOLoop.instance().stop()

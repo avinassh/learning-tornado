5f54e1bed3f8360b87044133fe58bbb41bbb8a9b
avinassh <avinashsajjan@gmail.com>
2013-12-13 15:16:58 +0530

    capture groups in POST request

--- tornado-book/1-2-string-rev.py
+++ tornado-book/1-2-string-rev.py
@@ -1,6 +1,12 @@
 
 #!/bin/python
 
+# Sample POST request : curl http://localhost:8000/wrap -d text=Lorem+ipsum+dolor+sit+amet,+consectetuer+adipiscing+elit
+# Output : Lorem ipsum dolor sit amet, consectetuer
+# 
+# Sample GET request : curl http://localhost:8000/reverse/helbhelb
+# Output : blehbleh
+#
 import textwrap
 
 # bunch of tornado imports
@@ -17,6 +23,7 @@ define("author", default='avi', help="the author of this server", type=str)
 
 class ReverseHandler(tornado.web.RequestHandler):
     # here the input varible is content of capture group
+    # /reverse/(\w+)
     # /reverse/string, string is sent as an parameter whenever there is 
     # GET request 
     def get(self, input):
@@ -36,6 +43,21 @@ class WrapHandler(tornado.web.RequestHandler):
         self.write("Damn bitch! You caused a %d error." % status_code)     
 
 
+class WrapHandlerWithInput(tornado.web.RequestHandler):
+    # This handler is to test whether POST request can also make use of
+    # capture group. And when the ulr is /wrap/sometsring the request will
+    # be handled by this method. 
+    # The content of capture group is sent as parameter to this method
+    def post(self, input):
+        text = self.get_argument('text')
+        width = self.get_argument('width', 40) 
+        response = 'You have given this: %s in input group\n\
+        and the Output is %s' % (input, textwrap.fill(text, width))
+        self.write(response)
+
+    def write_error(self, status_code, **kwargs):
+        self.write("Damn bitch! You caused a %d error." % status_code)
+
 if __name__ == "__main__": 
     tornado.options.parse_command_line()
     # Here regex has a capture group (i.e., a portion of the regex is
@@ -47,7 +69,8 @@ if __name__ == "__main__":
     app = tornado.web.Application(
         handlers=[
             (r"/reverse/(\w+)", ReverseHandler), 
-            (r"/wrap", WrapHandler)
+            (r"/wrap", WrapHandler),
+            (r"/wrap/(\w+)", WrapHandlerWithInput)
         ])
     http_server = tornado.httpserver.HTTPServer(app) 
     http_server.listen(options.port) 

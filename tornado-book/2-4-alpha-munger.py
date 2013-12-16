29e77c8f4c7248caae174ed2743c0b97e46e7f2a
avinassh <avinashsajjan@gmail.com>
2013-12-16 16:57:19 +0530

    modified code according to my needs

--- tornado-book/2-4-alpha-munger.py
+++ tornado-book/2-4-alpha-munger.py
@@ -16,21 +16,31 @@ class IndexHandler(tornado.web.RequestHandler):
 
 
 class MungedPageHandler(tornado.web.RequestHandler): 
-    def map_by_first_letter(self, text):
-        mapped = dict()
-        for line in text.split('\r\n'):
-            for word in [x for x in line.split(' ') if len(x) > 0]: 
-                if word[0] not in mapped: mapped[word[0]] = [] 
-                mapped[word[0]].append(word)
-        return mapped
+    # def map_by_first_letter(self, text):
+    #     mapped = dict()
+    #     for line in text.split('\r\n'):
+    #         for word in [x for x in line.split(' ') if len(x) > 0]: 
+    #             if word[0] not in mapped: mapped[word[0]] = [] 
+    #             mapped[word[0]].append(word)
+    #     return mapped
+
+    def word_doubler(self, text):
+        doubled_words = ''
+        for word in text.split():
+            doubled_words += 2 * word + ' '
+        return doubled_words    
+
 
     def post(self):
         source_text = self.get_argument('source')
-        text_to_change = self.get_argument('change')
-        source_map = self.map_by_first_letter(source_text)
-        change_lines = text_to_change.split('\r\n')
-        self.render('munged.html', source_map=source_map, 
-            change_lines=change_lines, choice=random.choice)
+        #text_to_change = self.get_argument('change')
+        #source_map = self.map_by_first_letter(source_text)
+        #change_lines = text_to_change.split('\r\n')
+        doubled_words = self.word_doubler(source_text)
+        #self.render('munged.html', source_text=source_map, 
+        #    change_lines=change_lines, choice=random.choice)
+        self.render('munged.html', source_text=source_text, 
+            doubled_words=doubled_words)
 
 
 if __name__ == '__main__': 

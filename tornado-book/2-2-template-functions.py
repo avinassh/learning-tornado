9656e8140d8a347e185700bf0b00f0e679265fbd
avinassh <avinashsajjan@gmail.com>
2013-12-16 12:57:09 +0530

    added url_escape and unescape

--- tornado-book/2-2-template-functions.py
+++ tornado-book/2-2-template-functions.py
@@ -15,3 +15,15 @@ print tornado.escape.xhtml_escape('<This is a test & it runs?>')
 print tornado.escape.xhtml_unescape('&lt;This is a test &amp; it runs?&gt;')
 # <This is a test & it runs?>
 
+# tornado.escape.url_escape(value) returns the input in URL-enocoded format
+# it internally uses urllib.quote_plus 
+# Default plus = True is sent as parameter to represent spaces as + instead 
+# of %20 
+
+print tornado.escape.url_escape('http://example.com/test test test')
+# http%3A%2F%2Fexample.com%2Ftest+test+test
+
+print tornado.escape.url_escape('http://example.com/test test test', plus=False)
+# http%3A//example.com/test%20test%20test
+
+# tornado.escape.url_unescape() is also available with extra parameter plus

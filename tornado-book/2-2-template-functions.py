1213512f2b89bf69c5c3451534c8ad7dfe64cc8b
avinassh <avinashsajjan@gmail.com>
2013-12-16 12:56:11 +0530

    added xhtml_escape and unescape functions

--- tornado-book/2-2-template-functions.py
+++ tornado-book/2-2-template-functions.py
@@ -3,3 +3,15 @@
 # To study and understand some of the template fucntions
 # escaping, JSONing and squeezing
 # url : http://www.tornadoweb.org/en/stable/escape.html
+
+import tornado.escape
+
+# tornado.escape.xhtml_escape(value) replaces &, < and > in the input string
+# with their corresponding HTML entities
+
+print tornado.escape.xhtml_escape('<This is a test & it runs?>')
+# &lt;This is a test &amp; it runs?&gt;
+
+print tornado.escape.xhtml_unescape('&lt;This is a test &amp; it runs?&gt;')
+# <This is a test & it runs?>
+
